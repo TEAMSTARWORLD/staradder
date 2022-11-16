5dce80c6dfd812c70ca2b2a95b8b8e6db1a55c9e
тєαм ѕтαя <115808731+TEAMSTARWORLD@users.noreply.github.com>
2022-11-16 20:21:38 +0530

    Update add.py

--- add.py
+++ add.py
@@ -1,7 +1,7 @@
 '''
 =============SON OF GENISYS=====================
 Astra members adding script
-Coded by a dumbass kid- github.com/TEAMSTARWORLD
+Coded by a dumbass kid- github.com/Cryptonian007
 Apologies if anything in the code is dumb :)
 Copy with credits
 ************************************************
